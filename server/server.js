e0bf7784bced2f0ffbe80e7e2224bcae08439b5f
Nuno Job <nunojobpinto@gmail.com>
2012-07-27 20:12:09 +0100

    nuno does changes

--- server/server.js
+++ server/server.js
@@ -2,11 +2,26 @@
 
 var path = require('path'),
     http = require('http'),
+    url = require('url'),
     static = require('node-static');
 
 var file = new(static.Server)('.');
 
 http.createServer(function (request, response) {
+  
+  var selected_file
+    , match_root = /^\/$/.exec(request.url)
+    , match_members = /^\/(members|about)\/(.*)/.exec(request.url)
+    , match_class = /^\/(essential-nodejs-training-course)/.exec(request.url)
+    ;
+  
+  if (match_root) {
+    request.url = "index.html"
+  } else if (match_members) {
+    request.url = "members/" + match_members[2].replace("+", "") + ".html";
+  } else if (match_class) {
+    request.url = match_class[1] + ".html";
+  }
 
   console.log(request.url);
   // if (request.url === '/') {
@@ -26,3 +41,5 @@ http.createServer(function (request, response) {
 
 }).listen(8080);
 
+
+console.log("8080");
